c690d8ba021b3eb54f901e2414a497d376fa63a2
joaoluchetta <joaoluchetta.dev@gmail.com>
2025-09-18 17:45:30 -0300

    Remove platforma windows, mac e tyzen do projeto

--- obj/Debug/net9.0-ios/ios-arm64/ProjetoMAUI.AssemblyInfo.cs
+++ obj/Debug/net9.0-ios/ios-arm64/ProjetoMAUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoMAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fe3475d4c88228706ea762e13eb891aee4e60cc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoMAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoMAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
